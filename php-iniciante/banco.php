0bf2da5cb61bb0f848df6eb7f5ed49358dd60e58
LDS-MAC <dogakiuchi@gmail.com>
2022-02-17 19:49:20 -0300

    Functions and Lists

--- php-iniciante/banco.php
+++ php-iniciante/banco.php
@@ -1,29 +1,6 @@
 <?php
 
-function printMessage($message)
-{
-    echo $message . PHP_EOL;
-}
-
-function deposit(array $account, float $value)
-{
-    if ($value < 0){
-        printMessage("Incorrect value!");
-    }else{
-        $account['balance'] += $value;
-    }
-        return $account;
-}
-
-function withdraw(array $account, float $value)
-{
-    if ($value > $account['balance']){
-        printMessage("Insufficient balance!");
-    }else{
-        $account['balance'] -= $value;
-    }
-        return $account;
-}
+require_once 'funcoes.php';
 
 $contaCorrente = [
     '999.756.110-23' => [
@@ -46,5 +23,6 @@ $contaCorrente['987.654.321-10'] = deposit($contaCorrente['987.654.321-10'], 500
 
 printMessage("     CPF       | Holder  | Balance");
 foreach ($contaCorrente as $cpf => $conta){
-    printMessage($cpf . " | " . $conta['holder']. " | " . $conta['balance']);
+    ['holder'=> $titular, 'balance'=> $saldo] = $conta;
+    printMessage("$cpf $titular $saldo");
 }
---
+++ php-iniciante/funcoes.php
@@ -0,0 +1,26 @@
+<?php
+
+function printMessage($message)
+{
+    echo $message . PHP_EOL;
+}
+
+function deposit(array $account, float $value)
+{
+    if ($value < 0){
+        printMessage("Incorrect value!");
+    }else{
+        $account['balance'] += $value;
+    }
+        return $account;
+}
+
+function withdraw(array $account, float $value)
+{
+    if ($value > $account['balance']){
+        printMessage("Insufficient balance!");
+    }else{
+        $account['balance'] -= $value;
+    }
+        return $account;
+}
